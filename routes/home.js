0704cfcb9b9b342f127778187a9f36c34bf44aef
nawapol <nawapol.est@gmail.com>
2025-04-02 13:55:04 +0700

    chore : add sign for seaport order

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,16 @@ const { SHA256 } = require("crypto-js");
 const router = express.Router();
 const ethers = require("ethers");
 
+const EIP_712_ORDER_TYPE = {
+  Order: [
+    { name: "maker", type: "address" },
+    { name: "taker", type: "address" },
+    { name: "tokenId", type: "uint256" },
+    { name: "price", type: "uint256" },
+    { name: "expiry", type: "uint256" }
+  ]
+};
+
 const encrypt = (key, data) => {
   const dataBuffer = Buffer.from(data, "utf8");
   const encryptedData = crypto.privateEncrypt(
@@ -132,6 +142,32 @@ router.post("/sign/message", async (req, res, next) => {
   try {
     const wallet = new ethers.Wallet(key);
     const signature = await wallet.signMessage(convertObjToUnit8Array(payload));
+    return res.status(200).json({
+      signature,
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
+});
+
+router.post("/sign/seaport-order", async (req, res, next) => {
+  console.log(req.headers);
+  console.log(`req body : ${JSON.stringify(req.body)}`);
+  const { orderComponents, domainData, key } = req.body;
+  try {
+    const wallet = new ethers.Wallet(key);
+    let signature = await wallet.signTypedData(
+      domainData,
+      EIP_712_ORDER_TYPE,
+      orderComponents,
+    );
+    // Use EIP-2098 compact signatures to save gas.
+    if (signature.length === 132) {
+      signature = ethers.Signature.from(signature).compactSerialized;
+    }
     return res.status(200).json({
       signature,
     });
